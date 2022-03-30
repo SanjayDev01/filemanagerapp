fc8082ea7b3e8d5b0c4a75a7c4ddeb1722451289
Sanju Poojari <36479146+SanjayDev01@users.noreply.github.com>
2022-03-31 03:50:55 +0530

    Permission for storage manage added

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:filemanagerapp/utilities/auth_check.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -14,10 +17,36 @@ class _SplashScreenState extends State<SplashScreen> {
     // TODO: implement initState
     super.initState();
 
+    _hasAcceptedPermissions();
+
     Future.delayed(const Duration(seconds: 2), () {
       Navigator.pushReplacement(context,
           MaterialPageRoute(builder: (_) => const AuthenticationWrapper()));
     });
+  }
+
+  _requestPermission(Permission permission) async {
+    final status = await permission.request();
+
+    setState(() {
+      print(status);
+    });
+  }
+
+  Future<bool> _hasAcceptedPermissions() async {
+    if (Platform.isAndroid) {
+      if (await _requestPermission(Permission.storage) &&
+          // access media location needed for android 10/Q
+          await _requestPermission(Permission.accessMediaLocation) &&
+          // manage external storage needed for android 11/R
+          await _requestPermission(Permission.manageExternalStorage)) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
   }
 
   @override
