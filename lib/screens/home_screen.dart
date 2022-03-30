e7d6bdb61874a11629d0d3d3b1adf37f1ec26d8c
Sanju Poojari <36479146+SanjayDev01@users.noreply.github.com>
2022-03-31 02:39:37 +0530

    basic home screen UI done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:filemanagerapp/screens/folder.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,8 +9,101 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool isLocked = true;
+
+  moveToDirectory() {}
+  enterPass(BuildContext context) async {
+    showDialog(
+      context: context,
+      builder: (context) {
+        TextEditingController folderName = TextEditingController();
+        return Dialog(
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ListTile(
+                  title: TextField(
+                    controller: folderName,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    // try {
+                    //   // Create Folder
+                    //   await FileManager.createFolder(
+                    //       controller.getCurrentPath, folderName.text);
+                    //   // Open Created Folder
+                    //   controller.setCurrentPath =
+                    //       controller.getCurrentPath + "/" + folderName.text;
+                    //   print(controller.getCurrentPath);
+                    // } catch (e) {
+                    //   print(e);
+                    // }
+
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Enter Password'),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: const Text(
+          "File Manager",
+          style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.blueGrey),
+        ),
+      ),
+      body: Column(
+        children: [
+          Card(
+            child: ListTile(
+              leading: const Icon(Icons.folder_outlined),
+              title: const Text("Local Storage"),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const FolderScreen()));
+              },
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Card(
+            child: ListTile(
+              leading: const Icon(Icons.folder_outlined),
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text("Private Directory"),
+                  isLocked
+                      ? const Icon(Icons.lock)
+                      : const Icon(Icons.lock_open_outlined)
+                ],
+              ),
+              onTap: () {
+                isLocked ? enterPass(context) : moveToDirectory();
+              },
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
