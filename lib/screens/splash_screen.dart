4cd7b23ff011444615e58b0f76344eb9cfab41f0
Sanju Poojari <36479146+SanjayDev01@users.noreply.github.com>
2022-03-30 17:52:18 +0530

    splash screen done

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Icon(
+              Icons.folder_open,
+              color: Colors.blueGrey,
+              size: 90.0,
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              'File Manager',
+              style: TextStyle(
+                color: Colors.blueGrey,
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              'by Sanju Poojari',
+              style: TextStyle(
+                color: Colors.blueGrey,
+                fontSize: 15.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
